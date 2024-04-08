f582253d208efdd050f70cd500d2eba6b2dd68d8
xiangzichan <166213580+xiangzichan@users.noreply.github.com>
2024-04-08 15:59:38 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,10 +20,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '尘乡子', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '传统文化研究者', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://yingxiaoren.online', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '国学,易经,传统文化,紫微斗数,子平术,看相,风水,八字,阳宅,起名字', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
